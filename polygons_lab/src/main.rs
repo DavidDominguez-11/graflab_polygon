1ae1cc4e89e6682da6ec8c89a2123d6442b9e811
DavidDominguez-11 <dominguezdavid396@gmail.com>
2025-07-10 19:54:38 -0600

    Add complete p1

--- polygons_lab/src/main.rs
+++ polygons_lab/src/main.rs
@@ -1,3 +1,79 @@
+mod framebuffer;
+mod line;
+
+use raylib::prelude::*;
+use framebuffer::Framebuffer;
+use line::line;
+
 fn main() {
-    println!("Hello, world!");
+    let width = 1000;
+    let height = 1000;
+    let mut framebuffer = Framebuffer::new(width, height);
+
+    framebuffer.set_background_color(Color::new(50, 50, 100, 255));
+    framebuffer.clear();
+
+    let poligono1 = vec![
+        (165, 380), (185, 360), (180, 330), (207, 345), 
+        (233, 330), (230, 360), (250, 380), (220, 385), 
+        (205, 410), (193, 383)
+    ];
+
+    dibujar_poligono_relleno(&poligono1, &mut framebuffer, Color::YELLOW);
+    framebuffer.render_to_file("poligono1.bmp");
+}
+
+fn dibujar_poligono_relleno(poligono: &[(i32, i32)], framebuffer: &mut Framebuffer, fill_color: Color) {
+
+    framebuffer.set_current_color(Color::WHITE);
+    dibujar_contorno(poligono, framebuffer);
+
+    let min_y = poligono.iter().map(|&(_, y)| y).min().unwrap();
+    let max_y = poligono.iter().map(|&(_, y)| y).max().unwrap();
+
+    framebuffer.set_current_color(fill_color);
+
+    for y in min_y..=max_y {
+        let mut intersecciones = Vec::new();
+
+        for i in 0..poligono.len() {
+            let (x1, y1) = poligono[i];
+            let (x2, y2) = poligono[(i + 1) % poligono.len()];
+
+            if y1 == y2 {
+                continue;
+            }
+
+            if (y >= y1 && y < y2) || (y >= y2 && y < y1) {
+                let x = x1 + ((y - y1) as f32 * (x2 - x1) as f32 / (y2 - y1) as f32).round() as i32;
+                intersecciones.push(x);
+            }
+        }
+        intersecciones.sort();
+
+        for i in (0..intersecciones.len()).step_by(2) {
+            if i + 1 >= intersecciones.len() {
+                break;
+            }
+            let x_start = intersecciones[i];
+            let x_end = intersecciones[i + 1];
+            for x in x_start..=x_end {
+                framebuffer.set_pixel(x as u32, y as u32);
+            }
+        }
+    }
+}
+
+
+fn dibujar_contorno(poligono: &[(i32, i32)], framebuffer: &mut Framebuffer) {
+    for i in 0..poligono.len() {
+        let (x1, y1) = poligono[i];
+        let (x2, y2) = poligono[(i + 1) % poligono.len()];
+        
+        line(
+            framebuffer,
+            Vector2::new(x1 as f32, y1 as f32),
+            Vector2::new(x2 as f32, y2 as f32),
+        );
+    }
 }
